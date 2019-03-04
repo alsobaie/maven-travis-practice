75cb278226fd784a87b0e4953713f43556d203ee
msa3d2023 <msa3d2023@fastmail.com>
2019-03-03 17:20:37 -0700

    init update

---
+++ src/main/java/maven_practice/App.java
@@ -0,0 +1,13 @@
+package maven_practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
